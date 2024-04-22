2bee353beb11f9aa8d9f54ab5556cbecc1f28fca
Hadi <mhadimollataheri@gmail.com>
2024-04-22 17:24:04 +0330

    Add primary structure of the app -Add buttons -Give some styles _Make img responsive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import images from "../src/assets/images";
 
 function App() {
   return (
-    <div>
-      <img
-        src={images[1]}
-        alt="jungle"
-        className='w-84 h-96'
-      />
+    <div id="app-container" className="flex justify-center items-center mt-10 text-center">
+      <button
+        id="back-button"
+        className="flex justify-center items-center m-6 p-5 rounded-full bg-black text-white w-3 h-3 text-center"
+      >
+        {"<"}
+      </button>
+
+      <div id="images-container" className="w-2/4">
+        <img src={images[1]} alt="jungle" className="max-w-full h-auto block" />
+      </div>
+
+      <button
+        id="forward-button"
+        className="flex justify-center items-center m-6 p-5 rounded-full bg-black text-white w-3 h-3 text-center"
+      >
+        {">"}
+      </button>
     </div>
   );
 }
