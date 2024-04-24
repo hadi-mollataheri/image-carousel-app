e6a3021d70d3ec75413879da9424dcc459d2dd07
Hadi <mhadimollataheri@gmail.com>
2024-04-24 10:22:58 +0330

    Create basic logic of buttons and centered the arrows -Buttons now work but they will increment the index of image without condition -Used raw css in index.css for centering arrows inside the buttons, but I might change them to tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
+import { useState } from "react";
+
 import images from "../src/assets/images";
 
 function App() {
+  // Create state for the images so we can figure out a way to show
+  // one image at the time and go the next or previous image with
+  // clicking on buttons
+  // I will use the state to store one image element to it at time
+  // and then when user click on buttons i will increment or decrement
+  // the index of the images array element
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const handleForward = () => {
+    setImageIndex((prevImageIndex) => {
+      return prevImageIndex + 1;
+    });
+  };
+
+  const handleBackward = () => {
+    setImageIndex((prevImageIndex) => {
+      return prevImageIndex - 1;
+    });
+  };
+
   return (
     <div
       id="app-container"
       className="flex justify-center items-center mt-10 text-center"
     >
-      <button
-        id="back-button"
-        className="flex justify-center items-center m-6 p-5 rounded-full bg-black text-white w-3 h-3 text-center align-middle font-extrabold text-xl"
-      >
-        ⇽
+      <button id="backward-button" onClick={handleBackward}>
+        
       </button>
 
-      <div id="images-container" className="w-2/4">
-        <img src={images[1]} alt="jungle" className="max-w-full h-auto block" />
+      <div
+        id="images-container"
+        className="w-2/4 h-96 flex items-center overflow-hidden"
+      >
+        <img
+          src={images[imageIndex]}
+          alt="jungle"
+          className="max-w-full h-auto block object-contain"
+        />
       </div>
 
-      <button
-        id="forward-button"
-        className="flex justify-center items-center m-6 p-5 rounded-full bg-black text-white w-3 h-3 text-center align-middle font-extrabold text-xl"
-      >
-        ⇾
+      <button id="forward-button" onClick={handleForward}>
+        
       </button>
     </div>
   );
 }
 
 export default App;
+
+// To Fix:
+// *- When images changes the size of them will change too and this cause
+// the buttons to change their place vertically, fix this behavior
