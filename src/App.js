63632d297a79cb712b8aeee35072f8112b6ad415
Hadi <mhadimollataheri@gmail.com>
2024-04-25 10:00:42 +0330

    Make the logic for handlers more readable -Use ternary operator for condtions of buttons

--- src/App.js
+++ src/App.js
@@ -8,21 +8,15 @@ function App() {
 
   const handleForward = () => {
     setImageIndex((prevImageIndex) => {
-      prevImageIndex += 1;
-      // If the index was not the last index + 1(last image) then return it
-      if (prevImageIndex !== images.length) return prevImageIndex;
-      // Otherwise if the image was last image(index is at lastIndex + 1 and not exist) then return the first image
-      else return (prevImageIndex = 0);
+      // If last image is reached then go back to the first image again otherwise go to the next image
+      return prevImageIndex === images.length - 1 ? 0 : prevImageIndex +1;
     });
   };
 
   const handleBackward = () => {
     setImageIndex((prevImageIndex) => {
-      prevImageIndex -= 1;
-      // If image(index) was not the first image then return it
-      if (prevImageIndex !== -1) return prevImageIndex;
-      // Otherwise if the image was the first image(index is at -1 and not exist) then return the last image
-      else return (prevImageIndex = images.length - 1);
+      // If the first image is reached then go back to the last image Otherwise go to the previous image
+      return prevImageIndex === 0 ? images.length - 1 : prevImageIndex - 1;
     });
   };
 
